5a6a7f2b9cf3d211572b7cd7f671ecd395319921
gbPagano <guilhermebpagano@gmail.com>
2024-01-27 20:01:59 -0300

    adding some activation functions

---
+++ src/functions.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class ActivationFunction(ABC):  # pragma: no cover
+    @abstractmethod
+    def activate(self, x) -> float:
+        ...
+
+    @abstractmethod
+    def derivative(self, x) -> float:
+        ...
+
+
+class TanH(ActivationFunction):
+    def activate(self, x):
+        return np.tanh(x)
+
+    def derivative(self, x):
+        return 1 - self.activate(x) ** 2
+
+
+class Sigmoid(ActivationFunction):
+    def activate(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x):
+        return self.activate(x) * (1 - self.activate(x))
+
+
+class ReLU(ActivationFunction):
+    def activate(self, x):
+        return max(0, x)
+
+    def derivative(self, x):
+        return 1 if x >= 0 else 0
+
+
+class LeakyReLU(ActivationFunction):
+    def __init__(self, alpha=0.01):
+        self.a = alpha
+
+    def activate(self, x):
+        return max(self.a * x, x)
+
+    def derivative(self, x):
+        return 1 if x >= 0 else self.a
+
+
+class ELU(ActivationFunction):
+    def __init__(self, alpha=1.0):
+        self.a = alpha
+
+    def activate(self, x):
+        return x if x >= 0 else self.a * (np.exp(x) - 1)
+
+    def derivative(self, x):
+        return 1 if x >= 0 else self.activate(x) + self.a
+
+
+class ErrorFunc:
+    def get_error(self, desired, output):
+        return 0.5 * (desired - output) ** 2
--- src/main.py
+++ src/main.py
@@ -1,31 +1,7 @@
-from abc import ABC, abstractmethod
-
 import numpy as np
 
+from src.functions import ActivationFunction
 
-class ActivationFunction(ABC): # pragma: no cover
-    @abstractmethod
-    def activate(self, x) -> float:
-        ...
-
-    @abstractmethod
-    def derivative(self, x) -> float:
-        ...
-
-
-class Tanh(ActivationFunction):
-    def activate(self, x):
-        return np.tanh(x)
-    
-    def derivative(self, x):
-        return 1 - self.activate(x) ** 2
-
-class Sigmoid(ActivationFunction):
-    def activate(self, x):
-        return 1 / (1 + np.exp(-x))
-    
-    def derivative(self, x):
-        return self.activate(x) * (1 - self.activate(x))
 
 class Layer:
     def __init__(self, neurons: tuple[int], function: ActivationFunction):
@@ -39,49 +15,7 @@ class Layer:
         self.net = self.input.dot(self.weights.T) + self.bias
         self.output = self.function.activate(self.net)
         return self.output
-    
+
     def backward(self, alpha, error):
         self.delta = error * self.function.derivative(self.net)
         self.weights += np.array([self.delta]).T * np.array([self.input]) * alpha
-
-
-class ErrorFunc:
-    def get_error(self, desired, output):
-        return .5 * (desired - output)**2
-
-
-
-
-layer_a = Layer(neurons=(2, 2), function=Sigmoid())
-layer_a.weights = np.array([
-    [0.15, 0.2],
-    [0.25, 0.3],
-])
-layer_a.bias = np.array([
-    0.35, .35,
-])
-layer_b = Layer(neurons=(2, 2), function=Sigmoid())
-layer_b.weights = np.array([
-    [0.4, 0.45],
-    [0.5, 0.55],
-])
-layer_b.bias = np.array([
-    0.6, .6,
-])
-
-
-inputs = np.array([
-    0.05, .1
-])
-desired = np.array([
-    0.01, 0.99
-])
-
-
-
-out_a = layer_a.forward(inputs)
-
-out_b = layer_b.forward(out_a)
-
-err = ErrorFunc().get_error(desired, out_b)
-err_total = sum(err)
